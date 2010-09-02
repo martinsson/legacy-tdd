40b3cb86dede5a018154ddc0e050c50ea8317005
Johan Martinsson <jm1974@hotmail.com>
2010-09-02 11:35:32 +0200

    about to support 170 == 101

--- src/test/java/thycotic/codechallenge/ThirteenBaseTest.java
+++ src/test/java/thycotic/codechallenge/ThirteenBaseTest.java
@@ -1,8 +1,10 @@
 package thycotic.codechallenge;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -19,6 +21,33 @@ public class ThirteenBaseTest {
 
 	@Test public void thirteenInTenBaseIsTenInThirteenBase() throws Exception {
 		assertThat(convert(13), equalTo("10"));
+	}
+
+	@Test public void twentyInTenBaseIs17InThirteenBase() throws Exception {
+		assertThat(convert(20), equalTo("17"));
+	}
+	
+	@Test public void oneSevenZeroInTenBaseIs101InThirteenBase() throws Exception {
+		assertThat(convert(170), equalTo("101"));
+	}
+	@Test @Ignore("dont understand the problem yet") public void itHandlesMediumNumbersToo() throws Exception {
+		assertThat(convert(5006), equalTo("2381"));
+	}
+	
+	@Test public void oneSevenZeroIsDivable2TimesBy13() throws Exception {
+		assertThat(timesDivableBy13(14), equalTo(1));
+		assertThat(timesDivableBy13(12), equalTo(0));
+		assertThat(timesDivableBy13(170), equalTo(2));
+	}
+	
+	private Integer timesDivableBy13(int i) {
+		int times = -1;
+		int div = i;
+		while (div > 0) {
+			div = div /13;
+			times++;
+		}
+		return times;
 	}
 
 	private String convert(int tenBaseNumber) {
