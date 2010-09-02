fc211be71b4e46d857159d3fe20d25256014495b
Johan Martinsson <jm1974@hotmail.com>
2010-09-02 10:54:26 +0200

    started using div and remainder

--- src/test/java/thycotic/codechallenge/ThirteenBaseTest.java
+++ src/test/java/thycotic/codechallenge/ThirteenBaseTest.java
@@ -8,25 +8,28 @@ import org.junit.Test;
 
 public class ThirteenBaseTest {
 	@Test public void fiveInThirteenBaseEqualsFiveInTenBase() throws Exception {
-		int fiveInTenBase = 5;
-		assertThat(convert(fiveInTenBase), equalTo("5"));
+		assertThat(convert(5), equalTo("5"));
 	}
 
 	@Test public void tenInTenBaseIsXinThirteenBase() throws Exception {
-		int x = 10;
-		assertThat(convert(x), equalTo("x"));
+		assertThat(convert(10), equalTo("x"));
+		assertThat(convert(11), equalTo("y"));
+		assertThat(convert(12), equalTo("z"));
 	}
 
 	@Test public void thirteenInTenBaseIsTenInThirteenBase() throws Exception {
-		int x = 13;
-		assertThat(convert(x), equalTo("10"));
+		assertThat(convert(13), equalTo("10"));
 	}
 
 	private String convert(int tenBaseNumber) {
+		int div = tenBaseNumber / 13;
+		int rem = tenBaseNumber % 13;
 		if (tenBaseNumber < 10)
-			return String.valueOf(tenBaseNumber);
+			return String.valueOf(rem);
 		else if (tenBaseNumber == 10) return "x";
-		else if (tenBaseNumber == 13) return "10";
-		else throw new RuntimeException("not implemented");
+		else if (tenBaseNumber == 11) return "y";
+		else if (tenBaseNumber == 12) return "z";
+		else if (tenBaseNumber == 13) return String.valueOf(10*div);
+		else throw new RuntimeException("not implemented for " + tenBaseNumber);
 	}
 }
