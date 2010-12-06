87f5c1e1ed4a78a392d606c7e52852b8e9cb4e10
Johan Martinsson <jm1974@hotmail.com>
2010-12-06 22:57:11 +0100

    added empty test so that mvn test passes

--- src/test/java/ugly/legacy/tdd/SomeJunit4Test.java
+++ src/test/java/ugly/legacy/tdd/SomeJunit4Test.java
@@ -2,6 +2,12 @@ package ugly.legacy.tdd;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class SomeJunit4Test {
+    @Test
+    public void testNothing() throws Exception {
+        
+    }
 
 }
