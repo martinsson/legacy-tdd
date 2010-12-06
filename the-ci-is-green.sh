c205ee418e0b21b9af96b164945249e45bf72224
Johan Martinsson <jm1974@hotmail.com>
2010-12-06 22:46:00 +0100

    ci script

---
+++ the-ci-is-green.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+REPO=`git remote -v | awk '/(push)/ {print $2}'`
+
+git pull
+
+if [ ! -d ".privatebuild" ]; then
+   git clone . .privatebuild
+fi
+
+cd .privatebuild
+git fetch
+git reset --hard origin\/master
+git clean -xdf
+
+mvn install
+
+if [ $? -eq 0 ]; then
+   echo "publishing to: $REPO"
+   git push $REPO $BRANCH
+else
+   echo "Unable to build"
+   exit $?
+fi
+
