e777e9b02a968a2b21ba90f6bcd9334cbaea2081
Johan Martinsson <jm1974@hotmail.com>
2010-12-09 23:55:07 +0100

    got the filtering working

--- src/test/java/ugly/legacy/tdd/GrepTest.java
+++ src/test/java/ugly/legacy/tdd/GrepTest.java
@@ -1,13 +1,10 @@
 package ugly.legacy.tdd;
 
 import static org.junit.Assert.assertTrue;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-
-import org.junit.Assert;
 import org.junit.Test;
-
 
 public class GrepTest {
 
@@ -18,20 +15,36 @@ public class GrepTest {
         Grep grep = new Grep(inputFile, outPutFile);
         grep.grep("");
          
+        areSameFiles(inputFile, outPutFile);
+    }
+    
+    @Test
+    public void selectsLinesThatContainTheGivenPattern() throws Exception {
+        String outPutFile = "src/test/ressources/output.txt";
+        String expectedOutput = "src/test/ressources/johanOutput.txt";
+        String inputFile = "src/test/ressources/sample.txt";
+        
+        Grep grep = new Grep(inputFile, outPutFile);
+        grep.grep("Johan");
+        
+        areSameFiles(expectedOutput, outPutFile);
+            
+    }
+
+    private void areSameFiles(String inputFile, String outPutFile) throws FileNotFoundException {
         Scanner outputscanner = new Scanner(new File(outPutFile));
         Scanner inputscanner = new Scanner(new File(inputFile));
 
         boolean areSame = true;
-        while(outputscanner.hasNext() && inputscanner.hasNext()) {
-            String inputLine = inputscanner.next();
-            String outputLine = outputscanner.next();
+        while(outputscanner.hasNextLine() && inputscanner.hasNextLine()) {
+            String inputLine = inputscanner.nextLine();
+            String outputLine = outputscanner.nextLine();
             areSame &= inputLine.equals(outputLine);
         }
         
-        assertTrue(outputscanner.hasNext() == inputscanner.hasNext());
+        assertTrue(outputscanner.hasNextLine() == inputscanner.hasNextLine());
         assertTrue(areSame);
     }
-    
     
     
 }
