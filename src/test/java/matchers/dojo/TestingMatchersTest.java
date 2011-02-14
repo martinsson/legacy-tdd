c9840b18020142da14c49c39877579d816ce97e8
unknown <Johan@.(none)>
2011-02-14 22:44:10 +0100

    added instructions for every test, reorganized tests

--- src/test/java/matchers/dojo/TestingMatchersTest.java
+++ src/test/java/matchers/dojo/TestingMatchersTest.java
@@ -25,29 +25,14 @@ import org.junit.Test;
 
 public class TestingMatchersTest {
     
+	//replace with matcher
     @Test @Ignore
     public void stringPatterns() throws Exception {
         String helloString = HelpersForTheDojo.generateHelloString();
         assertTrue(helloString.startsWith("hello"));
     }
     
-    @Test @Ignore
-    public void allPossibleResultsArePresented() throws Exception {
-        int numberOfDice = 100;
-        List<Integer> manyRolls = new BadDice(numberOfDice).roll();
-        assertTrue(manyRolls.containsAll(asList(1, 2, 3, 4, 5, 6)));
-    }
-    
-    @Test @Ignore
-    public void aDieRollIsAlwaysBetweenOneAndSix() throws Exception {
-        int numberOfDice = 100;
-        List<Integer> manyRolls = new BadDice(numberOfDice).roll();
-        for (Integer roll : manyRolls) {
-            assertTrue(roll >= 1);
-            assertTrue(roll <= 6);
-        }
-    }
-    
+  //replace with one single matcher
     @Test @Ignore
     public void diceAreNotWeighted() throws Exception {
         Dice dice = new RoundingDice(1000);
@@ -58,15 +43,41 @@ public class TestingMatchersTest {
         assertTrue(average > 3.4);
     }
     
+  //replace with generic matcher
+    @Test @Ignore
+    public void allPossibleResultsArePresented() throws Exception {
+        int numberOfDice = 1000;
+        List<Integer> manyRolls = new BadDice(numberOfDice).roll();
+        List<Integer> possibleResults = asList(1, 2, 3, 4, 5, 6);
+		assertTrue(manyRolls.containsAll(possibleResults));
+    }
+    
+  //This test probably already passes, due to your previous fix, 
+  //but for-loops are a smell in tests, replace it with one or two matchers
+    @Test @Ignore
+    public void aDieRollIsAlwaysBetweenOneAndSix() throws Exception {
+        int numberOfDice = 1000;
+        List<Integer> manyRolls = new BadDice(numberOfDice).roll();
+        for (Integer roll : manyRolls) {
+            assertTrue(roll >= 1);
+            assertTrue(roll <= 6);
+        }
+    }
+    
+  /**
+   * matchers are often used with mocking as is already the case here.
+   * Be sure to read and understand the error message before solving the problem
+   */
     @Test @Ignore
     public void compressesByRemovingExtraWhiteSpaces() throws Exception {
         PrintWriter mockedWriter = mock(PrintWriter.class);
         PrintWriter compressingPrintWriter = new CompressingPrintWriter(mockedWriter );
-        compressingPrintWriter.write("Il   y a 3 espaces qui devraient tre rŽduits ˆ une seule" );
+        compressingPrintWriter.write("Il   y a 3 espaces qui devraient ï¿½tre rï¿½duits ï¿½ une seule" );
         verify(mockedWriter).write(argThat(startsWith("Il y a")));
     }
     
-    @Test 
+    // Make your own matcher : ofSize
+    @Test @Ignore
     public void weCanAddElementsToTheSet() throws Exception {
         Set<String> cards = new TreeSet<String>();
         cards.add("1 of clubs");
@@ -75,7 +86,7 @@ public class TestingMatchersTest {
         assertThat(cards, is(ofSize(2)));
     }
 
-    /* To test
+    /* Test drive the matcher, below are suggestions for the tests to write
     returnsFalseWhenTheSizeDiffersFromWhatIsExpected
     returnsTrueWhenTheSizeIsCorrect
     describesTheExpectedCondition
